39fc3f74a2e6d0e6af35e24850ae25950668a3c7
wang-ps <wangps@hotmail.com>
2022-07-22 09:12:00 +0800

    Add poly_warmup

--- lr_scheduler.py
+++ lr_scheduler.py
@@ -26,13 +26,26 @@ def cos_warmup(optimizer, flags):
     if epoch <= warmup:
       return epoch / warmup
     else:
+      lr_min = flags.lr_min
       ratio = (epoch - warmup) / (flags.max_epoch - warmup)
-      return 0.0001 + 0.49995 * (1 + math.cos(math.pi * ratio))
+      return lr_min + 0.5 * (1.0 - lr_min) * (1 + math.cos(math.pi * ratio))
+  return LR.LambdaLR(optimizer, lr_lambda)
+
+
+def poly_warmup(optimizer, flags):
+  def lr_lambda(epoch):
+    warmup = flags.warmp_epoch
+    if epoch <= warmup:
+      return epoch / warmup
+    else:
+      ratio = (epoch - warmup) / (flags.max_epoch - warmup)
+      return (1 - ratio) ** flags.lr_power
   return LR.LambdaLR(optimizer, lr_lambda)
 
 
 def get_lr_scheduler(optimizer, flags):
   lr_dict = {'cos': cos, 'step': multi_step, 'cos': cos, 'poly': poly,
-             'constant': constant, 'cos_warmup': cos_warmup}
+             'constant': constant, 'cos_warmup': cos_warmup,
+             'poly_warmup': poly_warmup}
   lr_func = lr_dict[flags.lr_type]
   return lr_func(optimizer, flags)
